b7fa436d8490e7c0033ef5816efc86d690ad527b
Thales Fiscus <83731970+ThalesF01@users.noreply.github.com>
2023-04-18 09:34:47 -0300

    ajustes projetos

--- src/components/body/projetos.jsx
+++ src/components/body/projetos.jsx
@@ -17,16 +17,15 @@ export default function projetos(){
     return(   
         <Container id="projetos">           
             <Titulo>PROJETOS</Titulo>           
-
-            <Projetos>
-                {enviaProjetos(Marvel,"Pagina de compras de HQs feita com a API da Marvel","React.js e Styled Components","https://desafio-neo-app.vercel.app/","https://github.com/ThalesF01/Desafio-NeoApp")}
-                {enviaProjetos(Conversor,"Conversor de Moedas feito em React.JS","Reacj.JS, JavaScript, HTML5, CSS3 e BootStrap", "https://conversor-moedas-react.vercel.app/", "https://github.com/ThalesF01/Conversor-Moedas-React")}
-                {enviaProjetos(Blog, "Blog feito com Node e MongoDB.", "Node.JS, JavaScript, HTML5, CSS3, BootStrap e MongoDB","#","https://github.com/ThalesF01/Blog-Node" )}
-                {enviaProjetos(Pousada,"Pagina de reserva de hotel inspirado em exercicio da plataforma DevMedia.","HTML5, CSS3 e JavaScript","https://pousada-teste.vercel.app/","https://github.com/ThalesF01/Pousada-Secreta")}
-                {enviaProjetos(Music,"Pagina de musicas no estilo spotify","HTML5, CSS3 e JavaScript","https://music-library-gray.vercel.app/","https://github.com/ThalesF01/Music-Library")}
-                {enviaProjetos(Nubank, "Pagina de login de usuário inspirado no Nubank", "HTML5 e CSS3", "https://pagina-login-nubank.vercel.app", "https://github.com/ThalesF01/Pagina-Login-Nubank")}
-                {enviaProjetos(Calculadora, "Calculadora", "HTML5 e CSS3", "https://thalesf01.github.io/Calculadora/", "https://github.com/ThalesF01/Calculadora")}
-            </Projetos>                        
-        </Container>                            
+                <Projetos>
+                    {enviaProjetos(Marvel,"Pagina de compras de HQs feita com a API da Marvel","React.js e Styled Components","https://desafio-neo-app.vercel.app/","https://github.com/ThalesF01/Desafio-NeoApp")}
+                    {enviaProjetos(Conversor,"Conversor de Moedas feito em React.JS","Reacj.JS, JavaScript, HTML5, CSS3 e BootStrap", "https://conversor-moedas-react.vercel.app/", "https://github.com/ThalesF01/Conversor-Moedas-React")}
+                    {enviaProjetos(Blog, "Blog feito com Node e MongoDB.", "Node.JS, BootStrap, MongoDB e Handlebars","#","https://github.com/ThalesF01/Blog-Node" )}
+                    {enviaProjetos(Pousada,"Pagina de reserva de hotel inspirado em exercicio da plataforma DevMedia.","HTML5, CSS3 e JavaScript","https://pousada-teste.vercel.app/","https://github.com/ThalesF01/Pousada-Secreta")}
+                    {enviaProjetos(Music,"Pagina de musicas no estilo spotify","HTML5 e CSS3","https://music-library-gray.vercel.app/","https://github.com/ThalesF01/Music-Library")}
+                    {enviaProjetos(Calculadora, "Calculadora", "HTML5, CSS3 e Javascript", "https://thalesf01.github.io/Calculadora/", "https://github.com/ThalesF01/Calculadora")}
+                    {enviaProjetos(Nubank, "Pagina de login de usuário inspirado no Nubank", "HTML5 e CSS3", "https://pagina-login-nubank.vercel.app", "https://github.com/ThalesF01/Pagina-Login-Nubank")}                
+                </Projetos>                        
+       </Container>                            
     )
 }
